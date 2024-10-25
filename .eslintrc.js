e09947556f74a8caeb04361ea0054ee88882c58c
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-10-25 21:10:17 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {},
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex h-dvh items-center justify-center">
+      <h1 className="text-8xl">Job tracker</h1>
+    </main>
+  );
+}
+
+export default App;
