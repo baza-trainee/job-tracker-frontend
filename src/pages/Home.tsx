b8be641e5c02703d719e3cfe6883c7ca082e688d
blednyikaklanaya <hootro34@gmail.com>
2024-11-12 00:27:46 +0200

    Removed unnecessary code, removed unnecessary call to Modal.tsx component in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,6 @@ import { Button } from "../components/buttons/Button/Button";
 import { LinkButton } from "../components/buttons/LinkButton/LinkButton";
 import { IconButton } from "../components/buttons/IconButton/IconButton";
 import { FormExample } from "../components/form/FormExample/FormExample";
-import { useAppDispatch } from "../store/hook";
-import { openModal } from "../store/slices/modalSlice/modalSlice";
 
 function Home() {
   const { t } = useTranslation();
@@ -12,7 +10,6 @@ function Home() {
     console.log("click");
   };
 
-  const dispatch = useAppDispatch();
   return (
     <section className="p-4">
       <div className="container">
@@ -23,7 +20,7 @@ function Home() {
         <Button variant="ghost" disabled size="big" onClick={handleButtonClick}>
           Click me
         </Button>
-        <Button variant="ghost" size="small" onClick={() => dispatch(openModal({typeModal:"confirm", content: "Hello?", onCallFunction: () => {console.log("Ke")}}))}>
+        <Button variant="ghost" size="small" onClick={handleButtonClick}>
           Click me
         </Button>
         <LinkButton variant="ghost" size="big" href="/">
