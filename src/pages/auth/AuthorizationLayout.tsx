fd6d0d31be22db2062e8740d7dbd80915ec716e5
AlexBondar <alexbondar298@gmail.com>
2024-11-11 21:03:17 +0200

    reset commit

--- src/pages/auth/AuthorizationLayout.tsx
+++ src/pages/auth/AuthorizationLayout.tsx
@@ -8,8 +8,8 @@ import { useAuthForm } from "./use-auth";
 import LoginCardImages from "./login-images/login-card-images";
 import { Input } from "../../components/inputs/Input/Input";
 import { InputPassword } from "../../components/inputs/InputPassword/InputPassword";
-import Checkbox from "../../components/checkbox/Checkbox";
-import Separator from "../../components/separator/Separator";
+import Checkbox from "../../components/Checkbox/Checkbox";
+import Separator from "../../components/Separator/Separator";
 import { Button } from "../../components/buttons/Button/Button";
 
 //image
