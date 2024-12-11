003cd296bc05bcbea472fff4e9f65f3bcaa0cdd2
VickyBondarenko <quikyv@gmail.com>
2024-12-11 15:12:46 +0100

    Added footer to MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar/Sidebar";
+import Footer from "../components/layout/Footer";
 
 function MainLayout() {
   return (
     <div className="flex h-dvh w-full flex-col">
       <Sidebar />
-      <main className="ml-[92px] flex h-full">
+      <main className="ml-[92px] flex h-full flex-col justify-between">
         <Outlet />
+        <Footer />
       </main>
     </div>
   );
