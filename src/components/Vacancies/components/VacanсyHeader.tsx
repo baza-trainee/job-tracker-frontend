f77822e847a679540dae1eb6c4296c606cc19453
VickyBondarenko <quikyv@gmail.com>
2024-12-11 16:26:33 +0100

    Added basic searchInput markup

--- src/components/Vacancies/components/VacanсyHeader.tsx
+++ src/components/Vacancies/components/VacanсyHeader.tsx
@@ -1,11 +1,29 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import Icon from "../../Icon/Icon";
 
 const VacancyHeader: FC = () => {
-    return (
-        <div className="flex w-full items-center justify-center">
-            <h2 className="text-2xl">Vacancy filters and search</h2>
-        </div>
-    )
-}
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    console.log("form", form);
+  };
+  return (
+    <div className="flex w-full items-center justify-center pb-6">
+      {/* <h2 className="text-2xl">Vacancy filters and search</h2> */}
+      <form id="ahcnor1" className="r relative" onSubmit={handleSubmit}>
+        <input
+          className="flex w-[380px] items-center rounded-lg border-[1px] border-textBlack py-[10px] pl-14 text-xl leading-[135%] placeholder:text-[20px]"
+          type="text"
+          placeholder="Пошук"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <Icon id={"search"} className="absolute left-6 top-[14px] h-6 w-6" />
+      </form>
+    </div>
+  );
+};
 
 export default VacancyHeader;
