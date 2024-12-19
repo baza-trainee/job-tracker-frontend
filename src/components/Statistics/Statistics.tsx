e9ef22cce2a5d2ec8d463ee659e75e1ba8d4f91e
AlexBondar <alexbondar298@gmail.com>
2024-12-19 11:39:54 +0200

    ConfirmModal

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,7 +1,44 @@
+import {
+  notifyError,
+  notifyInfo,
+  notifySuccess,
+} from "../Notifications/NotificationService";
+import { useAppDispatch } from "../../store/hook";
+import { openModal } from "../../store/slices/modalSlice/modalSlice";
+import Modal from "../modal/Modal";
+
 function Statistics() {
+  const dispatch = useAppDispatch();
+
+
+  const handleToastError = () => {
+    notifyError("Щось пішло не так, дані не збережено");
+  };
+  const handleToastInfo = () => {
+    notifyInfo("Інформація");
+  };
+  const handleToastSuccess = () => {
+    notifySuccess("Звернення надіслано. Відповідь прийде на вказану пошту");
+  };
+
+  const handleModal = () => {
+    dispatch(
+      openModal({
+        typeModal: "contactUs",
+      })
+    );
+  };
   return (
-    <div className="flex w-full items-center justify-center">
+    // <div className="flex w-full items-center justify-center">
+    <div className="flex w-full flex-col items-center justify-center">
       <h2>Statistics</h2>
+      <div className="mt-60 flex flex-col gap-5">
+        <button onClick={() => handleToastError()}> Error</button>
+        <button onClick={() => handleToastInfo()}> Info</button>
+        <button onClick={() => handleToastSuccess()}> Success</button>
+        <button onClick={() => handleModal()}> modalButton</button>
+      </div>
+      <Modal/>
     </div>
   );
 }
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -10,8 +10,11 @@ import Icon from "../Icon/Icon.tsx";
 
 const Modal: FC = () => {
   const dispatch = useAppDispatch();
-  const { isModalOpen, typeModal } = useAppSelector((state) => state.modal);
-  const modalData = contentMap[typeModal]
+  const { isModalOpen, typeModal, isConfirmationOpen, typeConfirmation } =
+    useAppSelector((state) => state.modal);
+
+  const modalData = contentMap[typeModal || "close"];
+  const modalConfirmation = contentMap[typeConfirmation || "close"];
 
   useEffect(() => {
     const handleEscKey = (event: KeyboardEvent) => {
@@ -61,6 +64,35 @@ const Modal: FC = () => {
           </button>
         </div>
       </div>
+      {isConfirmationOpen ? (
+        <div className={clsx(
+            "fixed top-[25%] z-50",
+          )}>
+          <div
+            className={clsx(
+              "relative top-1 h-[30px] w-[130px] rounded-tl-xl rounded-tr-xl",
+              modalConfirmation.bgColor
+            )}
+          ></div>
+          <div
+            className={clsx(
+              "z-40 flex h-auto w-auto flex-row items-start justify-between rounded-lg rounded-tl-none border-4 bg-white p-4 shadow-form_shadow",
+              modalConfirmation.borderColor
+            )}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="flex min-h-[289px] min-w-[550px] items-center justify-center">
+              {modalConfirmation.content}
+            </div>
+            <button
+              onClick={() => dispatch(closeModal())}
+              className="rounded-md hover:bg-color2"
+            >
+              <Icon id="close-default" className="h-6 w-6 fill-textBlack" />
+            </button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
