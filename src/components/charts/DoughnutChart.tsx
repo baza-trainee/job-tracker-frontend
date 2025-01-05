3dc742c58dbe263e17a2ff827e8b119ea56ba4bc
VickyBondarenko <quikyv@gmail.com>
2025-01-05 20:58:50 +0100

    Added data to doughnutChat

--- src/components/charts/DoughnutChart.tsx
+++ src/components/charts/DoughnutChart.tsx
@@ -1,75 +1,27 @@
-import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+import { useTranslation } from "react-i18next";
+import { Vacancy } from "../../types/vacancies.types";
+import { getVacanciesByStatus } from "../Vacancies/components/VacanсyMainConfig";
+// import { useGetAllUserDataQuery } from "../../store/querySlices/profileQuerySlice";
+// import { getVacanciesByStatus } from "../Vacancies/components/VacanсyMainConfig";
+// import { FC } from "react";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "#C6E7FF",
-        "#A6AEBF",
-        "#CDC1FF",
-        "#FC8972",
-        "#FEEE91",
-        "#B1D690",
-      ],
-      borderColor: [
-        "#C6E7FF",
-        "#A6AEBF",
-        "#CDC1FF",
-        "#FC8972",
-        "#FEEE91",
-        "#B1D690",
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
-
-const options = {
-  responsive: true,
-  maintainAspectRatio: false,
-  plugins: {
-    legend: {
-      display: false,
-      position: "bottom",
-      align: "start",
-      labels: {
-        font: {
-          size: 16,
-        },
-        color: "#333",
-        //  boxWidth: 20,
-      },
-    },
-    tooltip: {
-      backgroundColor: "#FDFEFF",
-      borderColor: "#436B88",
-      borderWidth: 1,
-      titleColor: "#333",
-      bodyColor: "#ccc",
-    },
-  },
-};
+// getVacanciesByStatus(vacancies, "hr");
 
 const CustomLegend = ({ data }) => {
   return (
-    <div
-      style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}
-    >
+    <div className="flex h-[200px] w-[518px] flex-col flex-wrap justify-between gap-6 rounded-2xl bg-backgroundTertiary px-6 py-4">
       {data.labels.map((label, index) => (
         <div
           key={index}
           style={{
             display: "flex",
             alignItems: "center",
-            margin: "5px 10px",
-            width: "140px", // Ширина одного стовпчика
+
+            width: "223px", // Ширина одного стовпчика
           }}
         >
           <div
@@ -78,7 +30,7 @@ const CustomLegend = ({ data }) => {
               width: 70,
               height: 30,
               marginRight: 10,
-              borderRadius: 4, // Робимо маркер круглим
+              borderRadius: 4,
             }}
           ></div>
           <span>{label}</span>
@@ -88,9 +40,80 @@ const CustomLegend = ({ data }) => {
   );
 };
 
-export default function GoughnutChart() {
+export default function GoughnutChart({ vacancies }: { vacancies: Vacancy[] }) {
+  console.log("vacancies", vacancies);
+  const { t } = useTranslation();
+  //   const status = [
+  //     "sent",
+  //     "hrInterview",
+  //     "testTask",
+  //     "techInterview",
+  //     "rejection",
+  //     "offer",
+  //   ];
+  const status = ["resume", "hr", "test", "tech", "reject", "offer"];
+
+  const data = {
+    labels: status.map((s) => t(`sortDropdown.${s}`)),
+    datasets: [
+      {
+        label: "",
+        // data: [10, 19, 3, 5, 2, 3],
+        data: status.map(
+          (s) =>
+            vacancies.filter(
+              (v) => v.statuses && v.statuses[0] && v.statuses[0].name === s
+            ).length
+        ),
+        backgroundColor: [
+          "#C6E7FF",
+          "#CDC1FF",
+          "#FEEE91",
+          "#A6AEBF",
+          "#FC8972",
+          "#B1D690",
+        ],
+        borderColor: [
+          "#C6E7FF",
+          "#CDC1FF",
+          "#FEEE91",
+          "#A6AEBF",
+          "#FC8972",
+          "#B1D690",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+        position: "bottom",
+        align: "start",
+        labels: {
+          font: {
+            size: 16,
+          },
+          color: "#333",
+          //  boxWidth: 20,
+        },
+      },
+      tooltip: {
+        backgroundColor: "#FDFEFF",
+        borderColor: "#436B88",
+        borderWidth: 1,
+        titleColor: "#333",
+        bodyColor: "#ccc",
+      },
+    },
+  };
+
   return (
-    <div className="h-[400px] w-[400px]">
+    <div className="flex h-[400px] w-[478px] flex-col gap-6">
       <Doughnut data={data} options={options} />
       <CustomLegend data={data} />
     </div>
--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -3,8 +3,11 @@ import StatisticsCalendarTab from "../components/Calendar/StatisticsCalendarTab.
 import StatisticsCalendar from "../components/Calendar/StatisticsCalendar.tsx";
 import ChartBar from "../components/charts/ChartBar.tsx";
 import GoughnutChart from "../components/charts/DoughnutChart.tsx";
+import { useGetAllUserDataQuery } from "../store/querySlices/profileQuerySlice.ts";
 
 function Statistics() {
+  const { data, isLoading, isError } = useGetAllUserDataQuery();
+  const vacancies = data?.vacancies || [];
   return (
     <div className="container pb-8 pt-10">
       <StatisticsPanel />
@@ -14,7 +17,7 @@ function Statistics() {
           <StatisticsCalendar></StatisticsCalendar>
           <ChartBar></ChartBar>
         </div>
-        <GoughnutChart />
+        <GoughnutChart vacancies={vacancies} />
       </div>
     </div>
   );
