39a721400c9fbb800d5111657829a6640d111521
VickyBondarenko <quikyv@gmail.com>
2025-01-06 20:15:25 +0100

    Fixed option types errors

--- src/components/charts/DoughnutChart.tsx
+++ src/components/charts/DoughnutChart.tsx
@@ -1,11 +1,28 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  TooltipItem,
+  Legend,
+} from "chart.js";
+
 import { Doughnut } from "react-chartjs-2";
 import { useTranslation } from "react-i18next";
 import { Vacancy } from "../../types/vacancies.types";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const CustomLegend = ({ data }) => {
+interface ChartData {
+  labels: string[];
+  datasets: {
+    data: number[];
+    backgroundColor: string[];
+    borderColor: string[];
+    borderWidth: number;
+  }[];
+}
+
+const CustomLegend: React.FC<{ data: ChartData }> = ({ data }) => {
   return (
     <div className="flex h-[200px] w-[518px] flex-col flex-wrap justify-between gap-6 rounded-2xl bg-backgroundTertiary px-6 py-4">
       {data.labels.map((label, index) => (
@@ -35,68 +52,9 @@ const CustomLegend = ({ data }) => {
 };
 
 export default function GoughnutChart({ vacancies }: { vacancies: Vacancy[] }) {
-  console.log("vacancies", vacancies);
   const { t } = useTranslation();
 
   const status = ["resume", "hr", "test", "tech", "reject", "offer"];
-
-  //   const data = {
-  //     labels: status.map((s) => t(`sortDropdown.${s}`)),
-  //     datasets: [
-  //       {
-  //         label: "",
-  //         data: status.map(
-  //           (s) =>
-  //             vacancies.filter(
-  //               (v) => v.statuses && v.statuses[0] && v.statuses[0].name === s
-  //             ).length
-  //         ),
-  //         backgroundColor: [
-  //           "#C6E7FF",
-  //           "#CDC1FF",
-  //           "#FEEE91",
-  //           "#A6AEBF",
-  //           "#FC8972",
-  //           "#B1D690",
-  //         ],
-  //         borderColor: [
-  //           "#C6E7FF",
-  //           "#CDC1FF",
-  //           "#FEEE91",
-  //           "#A6AEBF",
-  //           "#FC8972",
-  //           "#B1D690",
-  //         ],
-  //         borderWidth: 1,
-  //       },
-  //     ],
-  //   };
-
-  //   const options = {
-  //     responsive: true,
-  //     maintainAspectRatio: false,
-  //     plugins: {
-  //       legend: {
-  //         display: false,
-  //         position: "bottom",
-  //         align: "start",
-  //         labels: {
-  //           font: {
-  //             size: 16,
-  //           },
-  //           color: "#333",
-  //           //  boxWidth: 20,
-  //         },
-  //       },
-  //       tooltip: {
-  //         backgroundColor: "#FDFEFF",
-  //         borderColor: "#436B88",
-  //         borderWidth: 1,
-  //         titleColor: "#333",
-  //         bodyColor: "#ccc",
-  //       },
-  //     },
-  //   };
 
   const data = status.map((s) =>
     Array.isArray(vacancies)
@@ -106,7 +64,6 @@ export default function GoughnutChart({ vacancies }: { vacancies: Vacancy[] }) {
       : 0
   );
 
-  // Загальна сума для розрахунку відсотків
   const total = data.reduce((sum, value) => sum + value, 0);
 
   const chartData = {
@@ -115,6 +72,7 @@ export default function GoughnutChart({ vacancies }: { vacancies: Vacancy[] }) {
       {
         label: "",
         data,
+        hoverBorderColor: "#436B88",
         backgroundColor: [
           "#C6E7FF",
           "#CDC1FF",
@@ -131,7 +89,7 @@ export default function GoughnutChart({ vacancies }: { vacancies: Vacancy[] }) {
           "#FC8972",
           "#B1D690",
         ],
-        borderWidth: 1,
+        borderWidth: 2,
       },
     ],
   };
@@ -142,8 +100,8 @@ export default function GoughnutChart({ vacancies }: { vacancies: Vacancy[] }) {
     plugins: {
       legend: {
         display: false,
-        position: "bottom",
-        align: "start",
+        // position: "bottom",
+        // align: "start",
 
         labels: {
           boxWidth: 40,
@@ -156,10 +114,10 @@ export default function GoughnutChart({ vacancies }: { vacancies: Vacancy[] }) {
         },
       },
       tooltip: {
-        xAlign: "left",
-        yAlign: "center",
+        xAlign: "left" as const,
+        yAlign: "center" as const,
         displayColors: false,
-        bodyAlign: "center",
+        bodyAlign: "center" as const,
         padding: 8,
         cornerRadius: {
           topLeft: 12,
@@ -176,19 +134,19 @@ export default function GoughnutChart({ vacancies }: { vacancies: Vacancy[] }) {
         titleFont: {
           size: 14,
           family: "Nunito",
-          weight: "bold ",
+          weight: "bold",
           lineHeight: 1.3,
-        },
+        } as const,
         bodyFont: {
           size: 14,
           family: "Nunito",
-          weight: "bold ",
+          weight: "bold",
           lineHeight: 1.3,
-        },
+        } as const,
         callbacks: {
-          label: function (tooltipItem) {
+          label: function (tooltipItem: TooltipItem<"doughnut">) {
             const dataset = tooltipItem.dataset;
-            const currentValue = dataset.data[tooltipItem.dataIndex];
+            const currentValue = dataset.data[tooltipItem.dataIndex] as number;
             const percentage = Math.round((currentValue / total) * 100);
             return `${percentage}%`;
           },
