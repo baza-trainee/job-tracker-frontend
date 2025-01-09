ed92c43dc642cb2f842a5a5c5d856d930e1f1e33
Olga Katolyk <orusyaeva1@gmail.com>
2025-01-10 01:36:11 +0200

    add styles for calendars of months and years

--- src/components/Calendar/StatisticsCalendarYear.tsx
+++ src/components/Calendar/StatisticsCalendarYear.tsx
@@ -3,13 +3,13 @@ import Icon from "../Icon/Icon";
 
 export const StatisticsCalendarYear = () => {
     return (
-        <div className="statistics-calendar w-[356px] box-border">
+        <div className="statistics-calendar w-[356px] h-auto box-border bg-backgroundTertiary px-2 py-6 rounded-[20px]">
             <Calendar
                 view="decade" // Відображає період з 12 років, показую тільки рік
                 className="custom-calendar"
                 nextLabel={<Icon id={"arrow-right"} className="size-6" />}
                 prevLabel={<Icon id={"arrow-left"} className="size-6" />}
-                // tileClassName="text-center py-4 px-6" // Стиль клітинок
+                tileClassName="w-[66px] h-[60px] font-Nunito text-textBlack bg-backgroundMain box-border" // Стиль клітинок
             />
         </div>
     );
