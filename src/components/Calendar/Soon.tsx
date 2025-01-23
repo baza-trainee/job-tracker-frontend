87a44555027ae4fb92d614580212644ca6ab35b9
Olga Katolyk <orusyaeva1@gmail.com>
2025-01-23 20:25:46 +0200

    styling statistic calendars and chartBar

--- src/components/Calendar/Soon.tsx
+++ src/components/Calendar/Soon.tsx
@@ -6,13 +6,13 @@ export const Soon = () => {
 
     return (
         <div className="w-[461px] flex flex-col p-4 text-textBlack">
-            <h3 className="w-full font-nunito px-6  text-textBlack text-2xl font-bold mb-[10px]">{t("soonSection.soon")}</h3>
+            <h3 className="w-full font-nunito px-6 text-textBlack text-[24px] font-medium mb-6">{t("soonSection.soon")}</h3>
             <ul className="w-full">
                 <li></li>
                 <li></li>
                 <li></li>
             </ul>
-            <button className="w-full py-3 px-6 bg-backgroundTertiary flex items-center justify-start gap-4 rounded-xl hover:bg-backgroundSecondary" >
+            <button className="w-full py-3 px-6 text-xl bg-backgroundTertiary flex items-center justify-start gap-4 rounded-xl hover:bg-backgroundSecondary" >
                 <Icon id="plus" className="size-6 ml-4 mr-[13px]" />
                 {t("soonSection.addEvent")}
             </button>
