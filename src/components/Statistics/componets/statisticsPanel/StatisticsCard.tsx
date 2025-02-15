4a3379eb7cca1e1b3960add907aa364bfd9d37e0
VickyBondarenko <quikyv@gmail.com>
2025-02-15 17:57:04 +0100

    Added adaptive styles for statistic panel

--- src/components/Statistics/componets/statisticsPanel/StatisticsCard.tsx
+++ src/components/Statistics/componets/statisticsPanel/StatisticsCard.tsx
@@ -8,7 +8,7 @@ const StatisticsCard: FC<StatisticsCardProps> = memo(
     return (
       <li
         className={cn(
-          "flex h-[77px] w-[134px] flex-col items-center justify-center rounded-2xl bg-color5 px-[29px] py-[6px] text-center even:bg-color1 md:h-[67px] md:w-[170px] md:p-1 xl:h-[128px] xl:w-[169px] xl:gap-1 xl:px-6 xl:py-4 2xl:h-[139px] 2xl:w-[192px] 2xl:gap-1 2xl:px-[35px] 2xl:py-4 3xl:h-[193px] 3xl:w-[259px] 3xl:gap-3 3xl:px-11 3xl:py-8"
+          "flex h-[77px] w-[134px] flex-col items-center justify-center rounded-2xl bg-color5 px-[29px] py-[6px] text-center md:h-[67px] md:w-[170px] md:p-1 xl:h-[128px] xl:w-[169px] xl:gap-1 xl:px-6 xl:py-4 xl:even:bg-color1 2xl:h-[139px] 2xl:w-[192px] 2xl:gap-1 2xl:px-[35px] 2xl:py-4 3xl:h-[193px] 3xl:w-[259px] 3xl:gap-3 3xl:px-11 3xl:py-8 smOnly:first:bg-color1 smOnly:last:bg-color1 mdOnly:first:bg-color1 mdOnly:last:bg-color1"
         )}
       >
         <h3 className="text-sm leading-[135%] md:text-base xl:text-xl 3xl:text-[28px] 3xl:leading-normal">
--- src/components/Statistics/componets/statisticsPanel/StatisticsPanel.tsx
+++ src/components/Statistics/componets/statisticsPanel/StatisticsPanel.tsx
@@ -42,7 +42,7 @@ const StatisticsPanel: FC<StatisticsPanelProps> = ({
         <div className="flex w-full flex-wrap gap-y-2 md:justify-between">
           <ul
             className={cn(
-              "flex flex-wrap gap-x-3 gap-y-2 md:gap-5 md:self-start"
+              "flex flex-wrap gap-x-3 gap-y-2 md:gap-2 xl:gap-3 2xl:gap-5 2xl:self-start 3xl:gap-8 mdOnly:w-1/2"
             )}
           >
             {panelList.map((item, index) => {
