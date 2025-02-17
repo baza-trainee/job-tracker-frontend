8176764e565ece1c02d42e3792d11547dc43dc3f
VickyBondarenko <quikyv@gmail.com>
2025-02-17 21:04:20 +0100

    Added adaptive styls for statisticPanel skeleton

--- src/components/Statistics/componets/statisticsPanel/PredictionCard.tsx
+++ src/components/Statistics/componets/statisticsPanel/PredictionCard.tsx
@@ -5,7 +5,7 @@ type PredictionCardProps = {
 
 const PredictionCard: React.FC<PredictionCardProps> = ({ header, text }) => {
   return (
-    <div className="flex h-[97px] w-full flex-col gap-2 rounded-2xl bg-color5 p-2 md:h-[142px] md:w-[348px] md:gap-1 md:p-4 xl:h-[128px] xl:w-[395px] xl:gap-2 2xl:h-[139px] 2xl:w-[447px] 2xl:gap-1 2xl:p-4 3xl:h-[193px] 3xl:w-[608px] 3xl:px-8 3xl:py-6">
+    <div className="flex h-[97px] w-full flex-col gap-2 rounded-2xl bg-color1 p-2 md:h-[142px] md:w-[348px] md:gap-1 md:p-4 xl:h-[128px] xl:w-[395px] xl:gap-2 xl:bg-color5 2xl:h-[139px] 2xl:w-[447px] 2xl:gap-1 2xl:p-4 3xl:h-[193px] 3xl:w-[608px] 3xl:px-8 3xl:py-6">
       <h5 className="text-xs leading-[135%] md:text-sm 2xl:text-base 3xl:text-xl">
         {header}
       </h5>
