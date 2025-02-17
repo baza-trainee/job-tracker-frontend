d7bd94b213387bd2fcc45ebf38a3d675d284e4ef
Olga Katolyk <orusyaeva1@gmail.com>
2025-02-17 12:07:20 +0200

    fix container in tailwind.config for all modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,11 +19,11 @@ export default {
         center: true,
         padding: {
           DEFAULT: "1.25rem",
-          // sm: "1.25rem",
-          // md: "1.25rem",
-          // xl: "1.25rem",
-          // "2xl": "1.25rem",
-          // "3xl": "1.25rem",
+          sm: "1rem 1.25rem 1.5rem",
+          md: "1.5rem",
+          xl: "2.5rem 1.5rem 2rem",
+          "2xl": "2.5rem 1.5rem 2rem",
+          "3xl": "2.5rem 1.5rem 2rem",
         },
       },
       boxShadow: {
