7d0ffc99d98a416ed3ada8bb0668e979afc01bf7
VickyBondarenko <quikyv@gmail.com>
2025-03-23 20:53:08 +0100

    Create node card

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -1,6 +1,9 @@
+import VacancyHeader from "@/components/Vacancies/components/VacanсyHeader";
+
 function Notes() {
   return (
     <div className="flex w-full items-center justify-center">
+      <VacancyHeader isArchive={false} />
       <h2>Notes</h2>
     </div>
   );
