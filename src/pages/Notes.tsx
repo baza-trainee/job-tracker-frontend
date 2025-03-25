706d43fdb723eb2b5f6ed07cfdf9cc9caf243fae
VickyBondarenko <quikyv@gmail.com>
2025-03-25 20:05:24 +0100

    Added structure for note page without data

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -1,10 +1,11 @@
+import NoVacancyCard from "@/components/Statistics/componets/statisticsPanel/NoVacancyCard";
 import VacancyHeader from "@/components/Vacancies/components/VacanсyHeader";
 
 function Notes() {
   return (
-    <div className="flex w-full items-center justify-center">
-      <VacancyHeader isArchive={false} />
-      <h2>Notes</h2>
+    <div className="container w-full pb-5 pt-4 sm:pb-6 sm:pt-4 md:py-6 xl:py-10">
+      <VacancyHeader isArchive={true} />
+      <NoVacancyCard />
     </div>
   );
 }
