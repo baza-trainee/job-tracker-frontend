9d77339aacbc7bc76b1b11b653879f22001bd3d6
Olga Katolyk <orusyaeva1@gmail.com>
2025-04-18 15:27:52 +0300

    add open modal addVacancy on FirstButton-card

--- src/components/Vacancies/components/VacancyCardFirst.tsx
+++ src/components/Vacancies/components/VacancyCardFirst.tsx
@@ -1,5 +1,10 @@
 import { FC } from "react";
+import { useAppDispatch } from "../../../store/hook.ts";
 import { useTranslation } from "react-i18next";
+import { openModal } from "../../../store/slices/modalSlice/modalSlice.ts";
+import { createNewStatuses } from "@/store/slices/statusVacancy/vacancyStatusSlice";
+import { vacancyStatusesInfo } from "@/store/slices/statusVacancy/vacancyStatusOperation";
+import { TypesModal } from "../../modal/ModalMain.types.ts";
 import clsx from "clsx";
 import Icon from "../../Icon/Icon.tsx";
 
@@ -7,14 +12,31 @@ export type VacancyCardFirstProps = {
   colorSectionBG: string;
   colorHoverBG: string;
   onClick?: () => void;
+  typeModal: TypesModal;
 };
 
 const VacancyCardFirst: FC<VacancyCardFirstProps> = ({
   colorSectionBG,
   colorHoverBG,
   onClick,
+  typeModal,
 }) => {
   const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+
+    dispatch(createNewStatuses(vacancyStatusesInfo));
+
+    dispatch(
+      openModal({
+        typeModal: typeModal,
+      })
+    );
+  };
 
   return (
     <button
@@ -25,7 +47,7 @@ const VacancyCardFirst: FC<VacancyCardFirstProps> = ({
         colorHoverBG,
         "w-[238px] smPlus:w-[212px] md:w-[198.67px] xl:w-[239px] 2xl:w-[276px] 3xl:w-[312.8px]"
       )}
-      onClick={onClick}
+      onClick={handleClick}
     >
       <div className="pl-[10px] xl:pl-7">
         <Icon id="plus" className="h-6 w-6" />
