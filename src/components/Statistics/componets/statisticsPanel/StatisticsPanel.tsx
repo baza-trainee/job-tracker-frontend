ac98e8bf6c576b7184ae7afac36e13e926181e1a
VickyBondarenko <quikyv@gmail.com>
2025-04-30 19:48:58 +0200

    Fixed statisticsPanel in desktop version and statisticsPanelSceleton in mobile

--- src/components/Statistics/componets/statisticsPanel/StatisticsPanel.tsx
+++ src/components/Statistics/componets/statisticsPanel/StatisticsPanel.tsx
@@ -40,7 +40,7 @@ const StatisticsPanel: FC<StatisticsPanelProps> = ({
     <div className="flex w-full flex-wrap gap-y-2 md:justify-between">
       <ul
         className={cn(
-          "flex flex-wrap gap-x-3 gap-y-2 smPlus:w-full md:gap-2 xl:gap-3 2xl:gap-5 2xl:self-start 3xl:gap-8 mdOnly:w-1/2"
+          "flex flex-wrap gap-x-3 gap-y-2 smPlus:w-full md:gap-2 xl:w-auto xl:gap-3 2xl:gap-5 2xl:self-start 3xl:gap-8 mdOnly:w-1/2"
         )}
       >
         {panelList.map((item, index) => {
--- src/components/Statistics/componets/statisticsPanel/StatisticsPanelSkeleton.tsx
+++ src/components/Statistics/componets/statisticsPanel/StatisticsPanelSkeleton.tsx
@@ -2,7 +2,7 @@ import Skeleton from "../../../Sceleton/Sceleton";
 
 const StatisticsCardSkeleton = () => {
   return (
-    <div className="flex h-[77px] w-[134px] flex-col items-center justify-center gap-1 rounded-2xl bg-color9-transparent px-[29px] py-[6px] text-center md:h-[67px] md:w-[170px] md:p-1 xl:h-[128px] xl:w-[169px] xl:gap-1 xl:px-6 xl:py-4 2xl:h-[139px] 2xl:w-[192px] 2xl:gap-1 2xl:px-[35px] 2xl:py-4 3xl:h-[193px] 3xl:w-[259px] 3xl:gap-3 3xl:px-11 3xl:py-8">
+    <div className="flex h-[77px] w-[calc(50%-6px)] flex-col items-center justify-center gap-1 rounded-2xl bg-color9-transparent px-[29px] py-[6px] text-center md:h-[67px] md:w-[170px] md:p-1 xl:h-[128px] xl:w-[169px] xl:gap-1 xl:px-6 xl:py-4 2xl:h-[139px] 2xl:w-[192px] 2xl:gap-1 2xl:px-[35px] 2xl:py-4 3xl:h-[193px] 3xl:w-[259px] 3xl:gap-3 3xl:px-11 3xl:py-8">
       <Skeleton width="80%" height="20%" />
       <Skeleton width="70%" height="20%" className="mdOnly:hidden" />
       <Skeleton width="30px" height="25%" className="mt-2 shrink-0 xl:mt-3" />
@@ -26,7 +26,7 @@ const PredictionCardSkeleton = () => {
 const StatisticsPanelSkeleton = () => {
   return (
     <div className="flex w-full flex-wrap gap-y-2 md:justify-between">
-      <div className="flex flex-wrap gap-x-3 gap-y-2 md:gap-2 xl:gap-3 2xl:gap-5 2xl:self-start 3xl:gap-8 mdOnly:w-1/2">
+      <div className="flex flex-wrap gap-x-3 gap-y-2 smPlus:w-full md:gap-2 xl:w-auto xl:gap-3 2xl:gap-5 2xl:self-start 3xl:gap-8 mdOnly:w-1/2">
         <StatisticsCardSkeleton />
         <StatisticsCardSkeleton />
         <StatisticsCardSkeleton />
