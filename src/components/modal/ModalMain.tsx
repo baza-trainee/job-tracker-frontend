94f3a11dbdbf99d793626633108de8bb6c13776c
AlexBondar <alexbondar298@gmail.com>
2025-05-18 16:06:25 +0300

    modal

--- src/components/modal/ModalMain.tsx
+++ src/components/modal/ModalMain.tsx
@@ -5,6 +5,7 @@ import { useAppSelector } from "@/store/hook";
 
 //alex
 import { useTranslation } from "react-i18next";
+import { forwardRef } from "react";
 
 type ModalDataProps = {
   content: ReactNode;
@@ -21,68 +22,75 @@ type ModalMainProps = {
   btnFunc: () => void;
 };
 
-const ModalMain = ({ className, modalData, btnFunc }: ModalMainProps) => {
-  const { borderColorModal, backgroundColorModal } = useAppSelector(
-    (state) => state.modal
-  );
-  const { t } = useTranslation();
+const ModalMain = forwardRef<HTMLDivElement, ModalMainProps>(
+  ({ className, modalData, btnFunc }, ref) => {
+    const { borderColorModal, backgroundColorModal } = useAppSelector(
+      (state) => state.modal
+    );
+    const { t } = useTranslation();
 
-  return (
-    <div className={className}>
-      <div
-        className={clsx(
-          "relative z-0 flex w-fit max-w-[330px] items-center justify-center rounded-tl-xl rounded-tr-xl",
-          "top-[1px] min-h-[20px] min-w-[100px] px-4 py-2",
-          "md:min-h-[32px] md:min-w-[134px] md:px-6 md:py-3",
-          backgroundColorModal || modalData?.bgColor || "bg-backgroundTertiary" // Тимчасово, пошукати далі
-        )}
-        onClick={(e) => e.stopPropagation()}
-      >
-        {modalData?.nameModal && (
-          <span className="text-[16px] font-bold text-textBlack md:text-[20px]">
-            {t(modalData.nameModal)}
-          </span>
-        )}
-      </div>
-      <div
-        className={clsx(
-          //alex mb-11
-          "z-0 mb-11 flex h-auto w-[280px] md:w-auto",
-          "flex-row items-start justify-between rounded-lg rounded-tl-none border-4 bg-backgroundTertiary p-2 shadow-form_shadow md:p-4",
-          // alex p-2 ---> md:p-5
-          "border-[2px]",
-          "md:border-[4px]",
-          borderColorModal || modalData?.borderColor || "bg-backgroundTertiary",
-          modalData?.paddingAddEventModal || "sm:p-2"
-        )}
-        onClick={(e) => e.stopPropagation()}
-      >
+    return (
+      <div className={className}>
         <div
           className={clsx(
-            "flex items-center justify-center",
-            "min-h-[217px] w-full min-w-[260px]",
-            "md:min-h-[237px] md:min-w-[449px]",
-            "xl:min-h-[292px] xl:min-w-[588px] xl:px-3",
-            "3xl:min-h-[320px]"
+            "relative z-0 flex w-fit max-w-[330px] items-center justify-center rounded-tl-xl rounded-tr-xl",
+            "top-[1px] min-h-[20px] min-w-[100px] px-4 py-2",
+            "md:min-h-[32px] md:min-w-[134px] md:px-6 md:py-3",
+            backgroundColorModal ||
+              modalData?.bgColor ||
+              "bg-backgroundTertiary" // Тимчасово, пошукати далі
           )}
+          onClick={(e) => e.stopPropagation()}
         >
-          {modalData?.content}
+          {modalData?.nameModal && (
+            <span className="text-[16px] font-bold text-textBlack md:text-[20px]">
+              {t(modalData.nameModal)}
+            </span>
+          )}
         </div>
-        <button
-          onClick={btnFunc}
-          className={"z-0 -ml-6 rounded-md md:hover:bg-color2"}
+        <div
+          ref={ref}
+          className={clsx(
+            //alex mb-11
+            "z-0 mb-11 flex h-auto w-[280px] md:w-auto",
+            "flex-row items-start justify-between rounded-lg rounded-tl-none border-4 bg-backgroundTertiary p-2 shadow-form_shadow md:p-4",
+            // alex p-2 ---> md:p-5
+            "border-[2px]",
+            "md:border-[4px]",
+            borderColorModal ||
+              modalData?.borderColor ||
+              "bg-backgroundTertiary",
+            modalData?.paddingAddEventModal || "sm:p-2"
+          )}
+          onClick={(e) => e.stopPropagation()}
         >
-          <Icon
-            id="close-default"
+          <div
             className={clsx(
-              "fill-textBlack",
-              modalData?.iconCloseEventModal || "h-6 w-6"
+              "flex items-center justify-center",
+              "min-h-[217px] w-full min-w-[260px]",
+              "md:min-h-[237px] md:min-w-[449px]",
+              "xl:min-h-[292px] xl:min-w-[588px] xl:px-3",
+              "3xl:min-h-[320px]"
             )}
-          />
-        </button>
+          >
+            {modalData?.content}
+          </div>
+          <button
+            onClick={btnFunc}
+            className={"z-0 -ml-6 rounded-md md:hover:bg-color2"}
+          >
+            <Icon
+              id="close-default"
+              className={clsx(
+                "fill-textBlack",
+                modalData?.iconCloseEventModal || "h-6 w-6"
+              )}
+            />
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default ModalMain;
