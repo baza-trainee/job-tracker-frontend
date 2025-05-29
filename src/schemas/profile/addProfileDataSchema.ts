b75e8e013aae647da52ede1863a3bf229091f376
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-05-29 20:24:34 +0200

    Update addProfileDataSchema.ts

--- src/schemas/profile/addProfileDataSchema.ts
+++ src/schemas/profile/addProfileDataSchema.ts
@@ -1,11 +1,11 @@
 import { PropsModalAddProperties } from "@/components/modal/components/profileModals/modalAddProperties.types";
-import { z, ZodObject, ZodOptional, ZodString } from "zod";
+import { z, ZodObject, ZodString } from "zod";
 
 type Schema = {
   [K in PropsModalAddProperties["cardsType"]]: ZodObject<{
     name: ZodString;
-    technologies: ZodOptional<ZodString>;
-    link: ZodOptional<ZodString>;
+    technologies: ZodString;
+    link: ZodString;
     text: any;
   }>;
 };
@@ -19,14 +19,13 @@ export const addProfileData: Schema = {
       })
       .max(50, { message: "validation.max50Characters" })
       .trim(),
-    technologies: z.string().optional(),
+    technologies: z.string(),
     link: z
       .string()
       .regex(/^https?:\/\/[a-zA-Z0-9-]{1,}(\.[a-zA-Z0-9-]{2,})+(\/[^\s]*)?$/, {
         message: "validation.invalidUrl",
       })
-      .trim()
-      .optional(),
+      .trim(),
     text: z.string().optional(),
   }),
 
@@ -39,15 +38,13 @@ export const addProfileData: Schema = {
     technologies: z
       .string()
       .trim()
-      .min(2, { message: "validation.min2Characters" })
-      .optional(),
+      .min(2, { message: "validation.min2Characters" }),
     link: z
       .string()
       .regex(/^https?:\/\/[a-zA-Z0-9-]{1,}(\.[a-zA-Z0-9-]{2,})+(\/[^\s]*)?$/, {
         message: "validation.invalidUrl",
       })
-      .trim()
-      .optional(),
+      .trim(),
     text: z.string().min(2, { message: "validation.min2Characters" }).trim(),
   }),
   addCoverLetters: z.object({
@@ -56,8 +53,8 @@ export const addProfileData: Schema = {
       .min(2, { message: "validation.min2Characters" })
       .max(50, { message: "validation.max50Characters" })
       .trim(),
-    technologies: z.string().optional(),
-    link: z.string().optional(),
+    technologies: z.string(),
+    link: z.string(),
     text: z
       .string()
       .trim()
@@ -72,19 +69,16 @@ export const addProfileData: Schema = {
     technologies: z
       .string()
       .min(2, { message: "validation.min2Characters" })
-      .trim()
-      .optional(),
+      .trim(),
     link: z
       .string()
       .trim()
       .regex(/^https?:\/\/[a-zA-Z0-9-]{1,}(\.[a-zA-Z0-9-]{2,})+(\/[^\s]*)?$/, {
         message: "validation.invalidUrl",
-      })
-      .optional(),
+      }),
     text: z
       .string()
       .trim()
-      .regex(/^.{2,}$/, { message: "validation.min2Characters" })
-      .optional(),
+      .regex(/^.{2,}$/, { message: "validation.min2Characters" }),
   }),
 };
