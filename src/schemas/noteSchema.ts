7cb64b61aa901b00499a807ec319a6bd13b4044e
AlexBondar <alexbondar298@gmail.com>
2025-06-01 12:53:40 +0300

    note

--- src/schemas/noteSchema.ts
+++ src/schemas/noteSchema.ts
@@ -1,20 +1,20 @@
 import { z } from "zod";
 
-import { nameContactUsRegex, textContactUsRegex } from "./regSchema";
+import { nameNoteRegex, textContactUsRegex } from "./regSchema";
 
 export const NoteSchema = z.object({
   noteName: z
     .string()
     .min(2, `contactUs.nameValidation.min`)
-    .regex(nameContactUsRegex, `notification.updatedUserName`)
-    .max(30, `contactUs.nameValidation.max`)
+    .regex(nameNoteRegex, `notification.updatedUserName`)
+    .max(100, `contactUs.nameValidation.max`)
     .trim()
     .or(z.literal("")),
   noteText: z
     .string()
     .min(10, `contactUs.textValidation.min`)
-    .regex(textContactUsRegex, `contactUs.textValidation.min`)
-    .max(4000, `contactUs.textValidation.text`)
+    .regex(textContactUsRegex, `contactUs.textValidation.text`)
+    .max(4000, `contactUs.textValidation.max`)
     .trim()
     .or(z.literal("")),
   noteType: z.enum(["addNote", "updateNote"]),
